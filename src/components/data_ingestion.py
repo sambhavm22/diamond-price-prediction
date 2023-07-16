4b67ebbf32d27129d8317b5f439933a6bce78254
sambhavm22 <58766591+sambhavm22@users.noreply.github.com>
2023-07-16 10:36:42 +0200

    EDA FE and model training file

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,8 @@
+import os, sys
+import numpy as np
+import pandas as pd
+
+from src.logger import logging
+from src.exception import CustomException
+
+from sklearn.model_selection import train_test_split
